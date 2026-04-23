8811e9c25593d34dd42983cb7d77a49b798fbf72
Ankit Sharma <sharmaji20202006@gmail.com>
2026-04-23 20:51:57 +0530

    Added login in create button to work

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-let searchbtn = document.querySelector("#search-btn");
-let searchinput = document.querySelector("#search-input");
+let searchBtn = document.querySelector("#search-btn");
+let searchInput = document.querySelector("#search-input");
+let notesContainer = document.getElementById("notes-container");
+let createBtn = document.querySelector("#create-btn");
+let noteTitle = document.getElementById("note-title");
+let noteContent = document.getElementById("note-content");
+
 
 // Search box text function
 function searchtext(){
-    let inputtext = searchinput.value; 
+    let inputtext = searchInput.value; 
     if(inputtext != ""){
         console.log(inputtext);
-        searchinput.value = "";
+        searchInput.value = "";
     };
 } 
-searchbtn.addEventListener("click", searchtext());
 
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        searchtext();
-    };
-});
+searchBtn.addEventListener("click", searchtext);
 
 
-let notesContainer = document.getElementById("notes-container");
 
 notesContainer.addEventListener("click", () => {
     notesContainer.classList.add("active");
@@ -28,4 +27,36 @@ document.addEventListener("click", (e) => {
     if (!notesContainer.contains(e.target)) {
         notesContainer.classList.remove("active");
     }
+});
+
+//logic for create button to work
+
+
+function notestext() {
+    let title = noteTitle.value;
+    let content = noteContent.value;
+
+    if (title.trim() !== "" && content.trim() !== "") {
+        console.log(title,"\n",content);
+
+        noteTitle.value = "";
+        noteContent.value = "";
+    }
+}
+
+createBtn.addEventListener("click" , notestext);
+
+
+
+
+
+
+
+//enter button press logic
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        searchtext();
+        notestext();
+    };
 });
